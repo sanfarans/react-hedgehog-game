70fcb9f74a6ec8f7c850959af4c69d1c22c3a631
sanfarans <jerzkra@gmail.com>
2022-11-19 18:51:32 +0100

    Display player curves properly

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,21 +1,51 @@
 export const TileTypes = {
     EMPTY: 0,
     POINT: 1,
-    HEAD: 2,
-    BODY: 3,
-    TAIL: 4
+
+    HEAD_UP: 2,
+    HEAD_DOWN: 3,
+    HEAD_LEFT: 4,
+    HEAD_RIGHT: 5,
+
+    BODY_HORIZONTAL: 6,
+    BODY_VERTICAL: 7,
+    BODY_LEFT_UP: 8,
+    BODY_LEFT_DOWN: 9,
+    BODY_RIGHT_UP: 10,
+    BODY_RIGHT_DOWN: 11,
+
+    TAIL_HORIZONTAL: 12,
+    TAIL_VERTICAL: 13
 };
 
 const tileTypeToCellContent = (type) => {
     switch(type) {
-        case TileTypes.HEAD:
-            return "H";
-        case TileTypes.BODY:
-            return "B";
-        case TileTypes.TAIL:
-            return "T";
         case TileTypes.POINT:
-            return "P";
+            return "o";
+        case TileTypes.HEAD_UP:
+            return "╿";
+        case TileTypes.HEAD_DOWN:
+            return "╽";
+        case TileTypes.HEAD_LEFT:
+            return "╾";
+        case TileTypes.HEAD_RIGHT:
+            return "╼";
+        case TileTypes.BODY_HORIZONTAL:
+            return "═";
+        case TileTypes.BODY_VERTICAL:
+            return "║";
+        case TileTypes.BODY_LEFT_UP:
+            return "╝";
+        case TileTypes.BODY_LEFT_DOWN:
+            return "╗";
+        case TileTypes.BODY_RIGHT_UP:
+            return "╚";
+        case TileTypes.BODY_RIGHT_DOWN:
+            return "╔"
+        case TileTypes.TAIL_HORIZONTAL:
+            return "┄";
+        case TileTypes.TAIL_VERTICAL:
+            return "┊";
         default:
             return "⠀";
     }
--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -32,16 +32,72 @@ const getNewPointLocation = (board, n = BOARD_SIZE) => {
     return possiblePlaces[Math.floor(Math.random()*possiblePlaces.length)];
 }
 
+const determineHead = (head, next) => {
+    const diffX = head.x - next.x;
+    const diffY = head.y - next.y;
+
+    if (diffX === 0) {
+        if (diffY === 1)
+            return TileTypes.HEAD_RIGHT;
+        return TileTypes.HEAD_LEFT
+    }
+    if (diffX === 1)
+        return TileTypes.HEAD_DOWN;
+    return TileTypes.HEAD_UP;
+}
+
+const determineBodyPart = (prev, current, next) => {
+    const diffX1 = prev.x - current.x;
+    const diffY1 = prev.y - current.y;
+    const diffX2 = current.x - next.x;
+    const diffY2 = current.y - next.y;
+
+    if (diffX1 === 0 && diffX2 === 0)
+        return TileTypes.BODY_HORIZONTAL
+    if (diffY1 === 0 && diffY2 === 0)
+        return TileTypes.BODY_VERTICAL
+    if (diffX1 === 0 && diffY1 === 1) { // right
+        if (diffX2 === 1) // up
+            return TileTypes.BODY_RIGHT_UP
+        return TileTypes.BODY_RIGHT_DOWN
+    }
+    if (diffX1 === 0 && diffY1 === -1) { // left
+        if (diffX2 === 1) // up
+            return TileTypes.BODY_LEFT_UP
+        return TileTypes.BODY_LEFT_DOWN
+    }
+    if (diffY1 === 0 && diffX1 === 1) { // down
+        if (diffY2 === 1) // left
+            return TileTypes.BODY_LEFT_DOWN
+        return TileTypes.BODY_RIGHT_DOWN
+    }
+    if (diffY1 === 0 && diffX1 === -1) { // up
+        if (diffY2 === 1) // left
+            return TileTypes.BODY_LEFT_UP
+        return TileTypes.BODY_RIGHT_UP
+    }
+    return TileTypes.BODY_HORIZONTAL;
+}
+
+const determineTail = (prev, tail) => {
+    if (prev.x === tail.x)
+        return TileTypes.TAIL_HORIZONTAL;
+    return TileTypes.TAIL_VERTICAL;
+}
+
 const generateBoardState = (playerHead, pointLocation, n = BOARD_SIZE) => {
     let board = [];
     for (let i = 0; i < n; ++i)
         board.push(new Array(n).fill(TileTypes.EMPTY))
-    board[playerHead.x][playerHead.y] = TileTypes.HEAD
+    board[playerHead.x][playerHead.y] = determineHead(playerHead, playerHead.next)
     let playerPart = playerHead.next;
+    let prev = playerHead;
     while (playerPart != null) {
-        board[playerPart.x][playerPart.y] = (
-            playerPart.next == null ? TileTypes.TAIL : TileTypes.BODY
-        )
+        if (playerPart.next == null)
+            board[playerPart.x][playerPart.y] = determineTail(prev, playerPart);
+        else
+            board[playerPart.x][playerPart.y] = determineBodyPart(prev, playerPart, playerPart.next);
+        prev = playerPart;
         playerPart = playerPart.next;
     }
     return board;
